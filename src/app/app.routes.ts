29186c70ce88a2a3d183273c85ef872b290efb7f
JuanManuel245 <juanmanuelcaffi@gmail.com>
2018-05-16 21:34:41 -0300

    Creado el front para agregar nuevas propiedades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { EmpresaComponent } from './components/empresa/empresa.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { VerComponent } from './components/admin/propiedades/ver/ver.component';
+import { NuevaComponent } from './components/admin/propiedades/nueva/nueva.component';
 
 const app_routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'empresa', component: EmpresaComponent },
   { path: 'administracion', component: AdminComponent },
   { path: 'administracion/ver', component: VerComponent },
+  { path: 'administracion/nueva', component: NuevaComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
 ];
 
